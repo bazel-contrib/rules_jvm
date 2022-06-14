bb12d34c87c07da04c4d0f87767a9617a21ddbd2
Boni Garcia <boni.garcia@uc3m.es>
2022-06-14 12:37:51 +0200

    Include tests using assumptions in JUnit 4 and 5

---
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/AssumptionsJUnit5Test.java
@@ -0,0 +1,18 @@
+package com.github.bazel_contrib.contrib_rules_jvm.junit5;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class AssumptionsJUnit5Test {
+
+    @Test
+    void shouldBeSkipped() {
+        Assumptions.assumeTrue(false);
+        System.out.println("<<<< shouldBeSkipped");
+    }
+
+    @Test
+    void shouldBeExecuted() {
+        System.out.println(">>>> shouldBeExecuted");
+    }
+}
