136078c7f587601b5b2c3479f04745010585e219
Thomas Jones-Low <tjoneslow@apple.com>
2022-06-23 14:10:41 -0400

    Fix Spotbug errors uncovered by the new version.

--- java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/TestRunningSecurityManagerTest.java
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/TestRunningSecurityManagerTest.java
@@ -3,8 +3,11 @@ package com.github.bazel_contrib.contrib_rules_jvm.junit5;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.security.Permission;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.junit.jupiter.api.Test;
 
+@SuppressFBWarnings("THROWS_METHOD_THROWS_CLAUSE_THROWABLE")
 public class TestRunningSecurityManagerTest {
 
   @Test
