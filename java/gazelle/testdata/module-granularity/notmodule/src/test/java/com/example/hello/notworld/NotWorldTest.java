b80f1c70dfe9837a6a2e8a11b746897f7f60d052
Daniel Wagner-Hall <dwagnerhall@apple.com>
2022-07-05 18:05:21 +0100

    Add test covering suite + not-module

---
+++ java/gazelle/testdata/module-granularity/notmodule/src/test/java/com/example/hello/notworld/NotWorldTest.java
@@ -0,0 +1,12 @@
+package com.example.hello.notworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class NotWorldTest {
+  @Test
+  public void notWorld() {
+    assertEquals("Not World!", NotWorld.NOT_WORLD);
+  }
+}
