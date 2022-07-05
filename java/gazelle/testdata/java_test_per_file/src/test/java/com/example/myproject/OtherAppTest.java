b05990d3ed3c352a580fa8cc8823432d6e33bc32
Daniel Wagner-Hall <dwagnerhall@apple.com>
2022-07-05 17:09:17 +0100

    Expand java_test_per_file test to two tests
    The test file has some TODOs to follow up on.

---
+++ java/gazelle/testdata/java_test_per_file/src/test/java/com/example/myproject/OtherAppTest.java
@@ -0,0 +1,15 @@
+package com.example.myproject;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class OtherAppTest {
+
+    @Test
+    public void testCompare() throws Exception {
+        App app = new App();
+        assertThat(app.compare(1, 1), equalTo(0));
+    }
+}
