3d956f60e20a80476f20ee8c5c263569282e38d7
Daniel Wagner-Hall <dwagnerhall@apple.com>
2022-07-05 17:53:24 +0100

    Add module test with per-file java_tests

---
+++ java/gazelle/testdata/java_test_per_file/src/test/java/com/example/somemodule/withdirectory/ATest.java
@@ -0,0 +1,16 @@
+package com.example.somemodule.withdirectory;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.myproject.App;
+
+import org.junit.Test;
+
+public class ATest {
+
+  @Test
+  public void testCompare() throws Exception {
+    App app = new App();
+    assertEquals("should return 0 when both numbers are equal", 0, app.compare(1, 1));
+  }
+}
