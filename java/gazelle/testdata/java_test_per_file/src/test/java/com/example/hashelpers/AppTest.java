eebab032167c2612fd68d6dfaea31a0dfc8227ef
Daniel Wagner-Hall <dwagnerhall@apple.com>
2022-07-06 16:28:22 +0100

    Cover (buggy) per-file non-module with helpers

---
+++ java/gazelle/testdata/java_test_per_file/src/test/java/com/example/hashelpers/AppTest.java
@@ -0,0 +1,14 @@
+package com.example.hashelpers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testCompare() throws Exception {
+        App app = new App();
+        assertEquals("should return 0 when both numbers are equal", 0, app.compare(Helper.powerOfOne(1), Helper.powerOfOne(1)));
+    }
+}
