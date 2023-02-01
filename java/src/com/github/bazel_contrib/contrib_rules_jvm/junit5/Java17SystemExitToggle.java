de767395ff4f86a98bfdccd8ad76fcf5ae4216df
Simon Mavi Stewart <simon_stewart@apple.com>
2023-02-01 15:33:46 +0000

    Do not fail the build if the security manager is disabled

--- java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/Java17SystemExitToggle.java
+++ java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/Java17SystemExitToggle.java
@@ -14,13 +14,7 @@ public class Java17SystemExitToggle extends Java11SystemExitToggle {
     Class<?> holderClazz = Class.forName("java.lang.System$CallersHolder");
     var callersField = holderClazz.getDeclaredField("callers");
 
-    // We want to avoid opening java.lang, so let's jump through hoops
-
-    // First problem: we can't call `Unsafe.getUnsafe` directly.
-    var c = Class.forName("sun.misc.Unsafe");
-    var lookup = MethodHandles.privateLookupIn(c, MethodHandles.lookup());
-    var handle = lookup.findStaticVarHandle(c, "theUnsafe", c);
-    var unsafe = (Unsafe) handle.get();
+    var unsafe = getUnsafe();
 
     // And we can't just grab the field easily
     var base = unsafe.staticFieldBase(callersField);
@@ -33,5 +27,39 @@ public class Java17SystemExitToggle extends Java11SystemExitToggle {
       Map<Class<?>, Boolean> map = Map.class.cast(callers);
       map.put(getClass().getSuperclass(), true);
     }
+  }
+
+  @Override
+  public void prevent() {
+    try {
+      super.prevent();
+    } catch (UnsupportedOperationException ignored) {
+      // Thrown when the security manager has been marked as unavailable
+      System.err.println(
+          "Unable to disable security manager. Calls to `System.exit`"
+              + " may succeed in tests. Some versions of java allow this behaviour to "
+              + "be overridden by setting the `java.security.manager` to `allow`");
+    }
+  }
+
+  @Override
+  public void allow() {
+    try {
+      super.allow();
+    } catch (UnsupportedOperationException ignored) {
+      // Thrown when the security manager has been marked as unavailable
+      // We have already warned the user about this in the call to `prevent`
+      // so silently fall through
+    }
+  }
+
+  private Unsafe getUnsafe() throws ReflectiveOperationException {
+    // We want to avoid opening java.lang, so let's jump through hoops
+
+    // First problem: we can't call `Unsafe.getUnsafe` directly.
+    var c = Class.forName("sun.misc.Unsafe");
+    var lookup = MethodHandles.privateLookupIn(c, MethodHandles.lookup());
+    var handle = lookup.findStaticVarHandle(c, "theUnsafe", c);
+    return (Unsafe) handle.get();
   }
 }
