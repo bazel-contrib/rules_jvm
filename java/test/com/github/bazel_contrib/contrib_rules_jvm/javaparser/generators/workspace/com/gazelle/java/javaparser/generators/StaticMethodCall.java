b73842f65d8beeb8e402a377b64483f1e167cc87
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-02-01 09:53:10 +0000

    Revert "Fully qualified static method calls (#81)"
    This reverts commit ea165afe466a1ee3d9d85ac017e4e8b1beb0dfe6.
    
    We've had reports that it breaks untested scenarios.

--- java/test/com/github/bazel_contrib/contrib_rules_jvm/javaparser/generators/workspace/com/gazelle/java/javaparser/generators/StaticMethodCall.java
+++
@@ -1,9 +0,0 @@
-package workspace.com.gazelle.java.javaparser.generators;
-
-public class StaticMethodCall {
-    public void Main() {
-        com.gazelle.java.javaparser.ClasspathParser.staticMethod();
-        com.gazelle.java.javaparser.ClasspathParser.InnerClass.staticMethod();
-        String.valueOf(1);
-    }
-}
