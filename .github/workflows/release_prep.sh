b46cf584bafa3165ddeac007113f398f6e22d8b3
Alex Eagle <alex@aspect.dev>
2023-02-10 06:23:58 -0800

    release: publish release archive
    GitHub's stability guarantee for the archive is iffy, and we want metrics on downloads.
    
    See https://github.com/bazel-contrib/SIG-rules-authors/issues/11#issuecomment-1411222578

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -4,8 +4,11 @@ set -eufo pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_jvm-${TAG:1}"
-SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+ARCHIVE="rules_jvm-$TAG.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
@@ -15,7 +18,7 @@ http_archive(
     name = "contrib_rules_jvm",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/bazel-contrib/rules_jvm/archive/refs/tags/${TAG}.tar.gz",
+    url = "https://github.com/bazel-contrib/rules_jvm/releases/download/${TAG}/${ARCHIVE}",
 )
 
 # Fetches the contrib_rules_jvm dependencies.
