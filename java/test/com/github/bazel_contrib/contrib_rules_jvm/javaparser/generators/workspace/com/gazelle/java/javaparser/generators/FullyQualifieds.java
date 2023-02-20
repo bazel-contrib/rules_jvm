9d4e1b44d708e6b1749bf0251836ddb4cdcc43e7
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-02-20 17:28:23 +0000

    Identify fully-qualified types
    This adds three contexts where we look for fully qualified types:
    * Fully-qualified static method calls
    * Fully-qualified constructor calls
    * Variable assignments with fully-qualified types
    
    It's a bit heuristic - it guesses what's a type based on standard
    conventions of class names.

---
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/javaparser/generators/workspace/com/gazelle/java/javaparser/generators/FullyQualifieds.java
@@ -0,0 +1,16 @@
+package workspace.com.gazelle.java.javaparser.generators;
+
+public class FullyQualifieds {
+    public void fn() {
+        new workspace.com.gazelle.java.javaparser.generators.DeleteBookRequest();
+        workspace.com.gazelle.java.javaparser.utils.Printer.print();
+
+        workspace.com.gazelle.java.javaparser.generators.DeleteBookResponse response = workspace.com.gazelle.java.javaparser.factories.Factory.create();
+
+        // instance methods shouldn't get detected as classes (e.g. this.foo).
+        this.foo.bar();
+
+        // Anonymous variables in lambdas shouldn't be picked up as variable names - visitVariable sees them as variables with null types.
+        someList.map(x -> x.toString());
+    }
+}
