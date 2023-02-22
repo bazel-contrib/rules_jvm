d16dc0adf9b2a20f1b2621a5f685e2880bcffbe9
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-02-22 10:09:34 -0600

    Blank out line numbers in test expectations
    Otherwise adding/removing lines to production code changes test
    expectations.

--- java/gazelle/private/testutil/reset_zerolog_timestamps/constant_logger.go
+++ java/gazelle/private/testutil/reset_zerolog_timestamps/constant_logger.go
@@ -10,4 +10,9 @@ func init() {
 	zerolog.TimestampFunc = func() time.Time {
 		return time.Unix(0, 0)
 	}
+
+	// Blank out line numbers, as otherwise our test expectations change whenever we edit production code.
+	zerolog.CallerMarshalFunc = func(file string, line int) string {
+		return file + ":XXX"
+	}
 }
