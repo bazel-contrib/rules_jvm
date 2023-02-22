b5ad3e08ba2e9fb8e427a2a567f35845eed744da
Daniel Wagner-Hall <dwagnerhall@apple.com>
2023-02-22 13:59:37 -0600

    Convert junit5 tests to be pure junit5
    Right now we're detecting both junit4 and junit5 features of these
    tests, because we're using Assert from junit4.
    
    Instead, make these tests pure junit5. We have specific tests for
    testing junit4/junit5/mixed behaviour.

--- java/gazelle/testdata/module-granularity/notmodule/src/test/java/com/example/hello/notworld/withhelpers/NotWorldWithHelpersTest.java
+++ java/gazelle/testdata/module-granularity/notmodule/src/test/java/com/example/hello/notworld/withhelpers/NotWorldWithHelpersTest.java
@@ -1,6 +1,6 @@
 package com.example.hello.hello.notworld.withhelpers;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
