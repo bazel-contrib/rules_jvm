e6097f876533adaff9ab94e20c2baff34d6c79f1
Farid Zakaria <fzakaria@confluent.io>
2024-06-25 14:01:31 -0700

    Java formatted XSLTTransformer.java

--- java/src/com/github/bazel_contrib/contrib_rules_jvm/xml/XSLTTransformer.java
+++ java/src/com/github/bazel_contrib/contrib_rules_jvm/xml/XSLTTransformer.java
@@ -1,35 +1,35 @@
+import java.io.File;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
 
 public class XSLTTransformer {
-    public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: java XSLTTransformer <input.xml> <transform.xslt>");
-            System.exit(1);
-        }
-
-        String inputXML = args[0];
-        String xsltFile = args[1];
-
-        // Create transformer factory
-        TransformerFactory factory = TransformerFactory.newInstance();
-
-        // Load the XSLT file
-        StreamSource xslt = new StreamSource(new File(xsltFile));
-
-        // Create a transformer
-        Transformer transformer = factory.newTransformer(xslt);
-
-        // Load the input XML file
-        StreamSource xmlInput = new StreamSource(new File(inputXML));
-
-        // Set the output file
-        StreamResult xmlOutput = new StreamResult(System.out);
-
-        // Perform the transformation
-        transformer.transform(xmlInput, xmlOutput);
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: java XSLTTransformer <input.xml> <transform.xslt>");
+      System.exit(1);
     }
+
+    String inputXML = args[0];
+    String xsltFile = args[1];
+
+    // Create transformer factory
+    TransformerFactory factory = TransformerFactory.newInstance();
+
+    // Load the XSLT file
+    StreamSource xslt = new StreamSource(new File(xsltFile));
+
+    // Create a transformer
+    Transformer transformer = factory.newTransformer(xslt);
+
+    // Load the input XML file
+    StreamSource xmlInput = new StreamSource(new File(inputXML));
+
+    // Set the output file
+    StreamResult xmlOutput = new StreamResult(System.out);
+
+    // Perform the transformation
+    transformer.transform(xmlInput, xmlOutput);
+  }
 }
