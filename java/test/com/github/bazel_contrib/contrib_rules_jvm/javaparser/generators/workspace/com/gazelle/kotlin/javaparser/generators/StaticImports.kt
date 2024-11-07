61e351df6be6ed7e392ae38f6058086279a88555
MorganR <morgan@roff.dev>
2024-11-07 23:15:07 +0000

    Start writing KtParserTest with some examples

---
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/javaparser/generators/workspace/com/gazelle/kotlin/javaparser/generators/StaticImports.kt
@@ -0,0 +1,10 @@
+package workspace.com.gazelle.kotlin.javaparser.generators
+
+import com.gazelle.java.javaparser.ClasspathParser.MAIN_CLASS
+import com.gazelle.kotlin.constantpackage.CONSTANT
+import com.gazelle.kotlin.constantpackage2.FOO
+import com.gazelle.kotlin.functionpackage.someFunction
+
+fun myFunction() {
+    someFunction()
+}
---
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/javaparser/generators/workspace/com/gazelle/kotlin/javaparser/generators/Wildcards.kt
@@ -0,0 +1,5 @@
+package workspace.com.gazelle.kotlin.javaparser.generators
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.*
