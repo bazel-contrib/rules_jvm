4e654eddc30884ef1b461d6127b1c02a692ad589
Simon Mavi Stewart <simon_stewart@apple.com>
2025-02-06 16:30:10 +0000

    Drop support for workspace-based builds

--- examples/workspace-based/java/src/com/github/bazelcontrib/rulesjvm/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.bazelcontrib.rulesjvm.example;
-
-import com.google.common.base.Joiner;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println(Joiner.on(" ").join("Hello,", "World!"));
-  }
-}
