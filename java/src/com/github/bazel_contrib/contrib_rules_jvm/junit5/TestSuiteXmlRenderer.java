2d0048815a609fdccf4be91b92e22d2f42eb9d27
Anshuman Mishra <a.mishra@uber.com>
2025-03-10 20:55:01 +0530

    run formatter

--- java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/TestSuiteXmlRenderer.java
+++ java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/TestSuiteXmlRenderer.java
@@ -4,16 +4,15 @@ import static com.github.bazel_contrib.contrib_rules_jvm.junit5.SafeXml.escapeIl
 import static com.github.bazel_contrib.contrib_rules_jvm.junit5.SafeXml.writeTextElement;
 
 import java.net.InetAddress;
-import java.time.format.DateTimeFormatter;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Locale;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
-
 import org.junit.platform.launcher.TestPlan;
 
 class TestSuiteXmlRenderer {
@@ -36,7 +35,7 @@ class TestSuiteXmlRenderer {
     DecimalFormat decimalFormat = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.ROOT));
     /* @Nullable */ Duration maybeDuration = suite.getDuration();
     Duration duration =
-            maybeDuration == null ? Duration.between(suite.getStarted(), Instant.now()) : maybeDuration;
+        maybeDuration == null ? Duration.between(suite.getStarted(), Instant.now()) : maybeDuration;
     xml.writeAttribute("time", decimalFormat.format(duration.toMillis() / 1000f));
 
     int errors = 0;
