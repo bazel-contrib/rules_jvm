bazel-contrib/rules_jvm

commit 8d835288e8d510f42a148f515a239cc32e165711

Add support for @AfterAll in XML report (#300)
Writing a simple test like
```java
package com.library;

import org.junit.jupiter.api.AfterAll;
import org.junit.jupiter.api.Test;

public class AlwaysFailTest {
    @Test
    public void doNothingTest() {
        System.out.println("Hi there!");
    }

    @AfterAll
    public static void alwaysFail() {
        throw new RuntimeException("Always failing.");
    }
}
```

Produces an XML that seems to look like it succeeds.
```xml
<?xml version="1.0" encoding="UTF-8"?>
<testsuites>
  <testsuite name="com.library.AlwaysFailTest" timestamp="2024-10-10T21:19:11.522176Z" hostname="KHK9NLVQGN" tests="2" failures="0" errors="0" disabled="0" skipped="0" package="">
    <properties/>
    <testcase name="doNothingTest" classname="com.library.AlwaysFailTest" time="0.03">
      <system-out><![CDATA[Hi there!
]]></system-out>
    </testcase>
  </testsuite>
</testsuites>
```

Bazel itself correctly identifies it fails. The Junit4 reporter also
included with Bazel natively correclty reports the failure in the XML
output.

Augment the Junit listener to add support for static methods and add
them to the JUnit output.

Doing so produces the following XML.
```xml
<?xml version="1.0" encoding="UTF-8"?>
<testsuites>
  <testsuite name="com.library.AlwaysFailTest" timestamp="2024-10-10T21:49:02.096648Z" hostname="KHK9NLVQGN" tests="3" failures="1" errors="0" disabled="0" skipped="0" package="">
    <properties/>
    <testcase name="com.library.AlwaysFailTest" classname="com.library.AlwaysFailTest" time="0.05">
      <failure message="Always failing." type="java.lang.RuntimeException"><![CDATA[java.lang.RuntimeException: Always failing.
	at com.library.AlwaysFailTest.alwaysFail(AlwaysFailTest.java:20)
...
	at com.github.bazel_contrib.contrib_rules_jvm.junit5.JUnit5Runner.main(JUnit5Runner.java:39)
]]></failure>
    </testcase>

java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/BazelJUnitOutputListener.java
@@ -109,7 +109,9 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
       // results
       List<UniqueId.Segment> segments = testCase.getId().getUniqueIdObject().getSegments();
 
-      if (segments.size() == 3 || segments.size() == 5) {
+      if (segments.size() == 2) {
+        parent = results.get(testCase.getId().getUniqueIdObject());
+      } else if (segments.size() == 3 || segments.size() == 5) {
         // get class / test data up one level
         parent =
             testCase
@@ -161,7 +163,7 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
               // are identified by the fact that they have no child test cases in the
               // test plan, or they are marked as tests.
               TestIdentifier id = result.getId();
-              return id.isTest() || testPlan.getChildren(id).isEmpty();
+              return id.getSource() != null || id.isTest() || testPlan.getChildren(id).isEmpty();
             })
         .collect(Collectors.toList());
   }

java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/BazelJUnitOutputListenerTest.java
@@ -5,29 +5,40 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 import static org.junit.platform.launcher.LauncherConstants.STDERR_REPORT_ENTRY_KEY;
 import static org.junit.platform.launcher.LauncherConstants.STDOUT_REPORT_ENTRY_KEY;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.UniqueId;
 import org.junit.platform.engine.reporting.ReportEntry;
+import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherConfig;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.testkit.engine.EngineTestKit;
 import org.mockito.Mockito;
 import org.opentest4j.TestAbortedException;
 import org.w3c.dom.Document;
@@ -37,10 +48,71 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-public class BazelJUnitOuputListenerTest {
+public class BazelJUnitOutputListenerTest {
 
   private TestDescriptor testDescriptor = new StubbedTestDescriptor(createId("descriptors"));
   private TestIdentifier identifier = TestIdentifier.from(testDescriptor);
+
+  /** This latch is used in TestAfterAllFails for testAfterAllFailuresAreReported */
+  private static final AtomicBoolean causeFailure = new AtomicBoolean(false);
+
+  static final class TestAfterAllFails {
+
+    @SuppressFBWarnings(value = "THROWS_METHOD_THROWS_RUNTIMEEXCEPTION")
+    @AfterAll
+    static void afterAll() {
+      if (causeFailure.get()) {
+        throw new RuntimeException("I always fail.");
+      }
+    }
+
+    @Test
+    void test() {}
+  }
+
+  @Test
+  public void testAfterAllFailuresAreReported() throws IOException {
+    causeFailure.set(true);
+
+    // First let's do a sanity test that we have the expected failures for the @AfterAll
+    EngineTestKit.engine("junit-jupiter")
+        .selectors(selectClass(TestAfterAllFails.class))
+        .execute()
+        .containerEvents()
+        .assertStatistics(stats -> stats.skipped(0).started(2).succeeded(1).aborted(0).failed(1));
+
+    // Now let's run the same test. Unfortunately we cannot use EngineTestKit since it has no way
+    // to register a listener.
+    File xmlFile = File.createTempFile("junit-report", "xml");
+    BazelJUnitOutputListener listener = new BazelJUnitOutputListener(xmlFile.toPath());
+    LauncherConfig config = LauncherConfig.builder().addTestExecutionListeners(listener).build();
+
+    Launcher launcher = LauncherFactory.create(config);
+
+    LauncherDiscoveryRequestBuilder request =
+        LauncherDiscoveryRequestBuilder.request().selectors(selectClass(TestAfterAllFails.class));
+
+    launcher.discover(request.build());
+
+    launcher.execute(request.build());
+    listener.close();
+
+    // now write an assertion to validate the XML file has an error
+    String[] expectedStrings = {
+      "<failure message=\"I always fail.\" type=\"java.lang.RuntimeException\">", "failures=\"1\"",
+    };
+
+    // Useful for debugging the expected output
+    // System.out.println(Files.readString(xmlFile.toPath()));
+
+    for (String expected : expectedStrings) {
+      assertTrue(
+          Files.lines(xmlFile.toPath()).anyMatch(line -> line.contains(expected)),
+          "Expected to find " + expected + " in " + xmlFile);
+    }
+
+    causeFailure.set(false);
+  }
 
   @Test
   public void testResultCanBeDisabled() {

MODULE.bazel
@@ -222,6 +222,7 @@ dev_maven.install(
         "org.junit.platform:junit-platform-suite:1.8.2",
         "org.junit.platform:junit-platform-suite-api:1.8.2",
         "org.junit.platform:junit-platform-suite-engine:1.8.2",
+        "org.junit.platform:junit-platform-testkit:1.8.2",
         "org.junit.vintage:junit-vintage-engine:5.8.2",
         "org.mockito:mockito-core:4.8.1",
     ],

java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/TestData.java
@@ -146,4 +146,25 @@ class TestData {
   public Instant getStarted() {
     return this.started;
   }
+
+  @Override
+  public String toString() {
+    return "TestData{"
+        + "id="
+        + id
+        + ", reportEntries="
+        + reportEntries
+        + ", started="
+        + started
+        + ", finished="
+        + finished
+        + ", reason='"
+        + reason
+        + '\''
+        + ", result="
+        + result
+        + ", dynamic="
+        + dynamic
+        + '}';
+  }
 }

java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/BUILD.bazel
@@ -38,6 +38,7 @@ java_test_suite(
         artifact("org.junit.jupiter:junit-jupiter-api", "contrib_rules_jvm_tests"),
         artifact("org.junit.jupiter:junit-jupiter-params", "contrib_rules_jvm_tests"),
         artifact("org.junit.platform:junit-platform-engine", "contrib_rules_jvm_tests"),
+        artifact("org.junit.platform:junit-platform-testkit", "contrib_rules_jvm_tests"),
         artifact("org.mockito:mockito-core", "contrib_rules_jvm_tests"),
         artifact("org.opentest4j:opentest4j", "contrib_rules_jvm_tests"),
     ] + junit5_vintage_deps("contrib_rules_jvm_tests"),

commit 1013ee4ebaa3419356217e941d2f6a51f40936eb

fix: BazelJUnitOutputListener logging xml output on suite-level failures (#328)
fixes: #327 

1. Updated code to propagate the test suite failure to each test case.
2. The test suite failure will be propagated to each child node, even if
the test is not executed.
3. Additionally, this change fixes the bug introduced in #300, which
incorrectly logged the test_suite as a testcase and logged incorrect
values for the "tests" attribute in the <testsuite> node.

java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/BazelJUnitOutputListener.java
@@ -5,6 +5,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -50,8 +51,12 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
   public BazelJUnitOutputListener(Path xmlOut) {
     try {
       Files.createDirectories(xmlOut.getParent());
-      BufferedWriter writer = Files.newBufferedWriter(xmlOut, UTF_8);
-      xml = XMLOutputFactory.newFactory().createXMLStreamWriter(writer);
+      // Use LazyFileWriter to defer file creation until the first write operation.
+      // This prevents premature file creation in cases where the JVM terminates abruptly
+      // before any content is written. If no output is generated, Bazel has custom logic
+      // to create the XML file from test.log, but this logic only activates if the
+      // output file does not already exist.
+      xml = XMLOutputFactory.newFactory().createXMLStreamWriter(new LazyFileWriter(xmlOut));
       xml.writeStartDocument("UTF-8", "1.0");
     } catch (IOException | XMLStreamException e) {
       throw new IllegalStateException("Unable to create output file", e);
@@ -140,8 +145,9 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
         throw new IllegalStateException(
             "Unexpected test organization for test Case: " + testCase.getId());
       }
-      if (includeIncompleteTests || testCase.getDuration() != null) {
-        knownSuites.computeIfAbsent(parent, id -> new ArrayList<>()).add(testCase);
+      knownSuites.computeIfAbsent(parent, id -> new ArrayList<>());
+      if (testCase.getId().isTest() && (includeIncompleteTests || testCase.getDuration() != null)) {
+        knownSuites.get(parent).add(testCase);
       }
     }
 
@@ -153,18 +159,19 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
   // This is really just documentation until someone actually turns on a static analyser.
   // If they do, we can decide whether we want to pick up the dependency.
   // @GuardedBy("resultsLock")
-  private List<TestData> findTestCases_locked() {
+  private List<TestData> findTestCases_locked(String engineId) {
     return results.values().stream()
         // Ignore test plan roots. These are always the engine being used.
         .filter(result -> !testPlan.getRoots().contains(result.getId()))
         .filter(
-            result -> {
-              // Find the test results we will convert to `testcase` entries. These
-              // are identified by the fact that they have no child test cases in the
-              // test plan, or they are marked as tests.
-              TestIdentifier id = result.getId();
-              return id.getSource() != null || id.isTest() || testPlan.getChildren(id).isEmpty();
-            })
+            result ->
+                engineId == null
+                    || result
+                        .getId()
+                        .getUniqueIdObject()
+                        .getEngineId()
+                        .map(engineId::equals)
+                        .orElse(false))
         .collect(Collectors.toList());
   }
 
@@ -196,20 +203,38 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
       return;
     }
 
-    output(false);
+    output(false, testIdentifier.getUniqueIdObject().getEngineId().orElse(null));
   }
 
-  private void output(boolean includeIncompleteTests) {
+  private void output(boolean includeIncompleteTests, /*@Nullable*/ String engineId) {
     synchronized (this.resultsLock) {
-      List<TestData> testCases = findTestCases_locked();
+      List<TestData> testCases = findTestCases_locked(engineId);
       Map<TestData, List<TestData>> testSuites =
           matchTestCasesToSuites_locked(testCases, includeIncompleteTests);
 
       // Write the results
       try {
         for (Map.Entry<TestData, List<TestData>> suiteAndTests : testSuites.entrySet()) {
-          new TestSuiteXmlRenderer(testPlan)
-              .toXml(xml, suiteAndTests.getKey(), suiteAndTests.getValue());
+          TestData suite = suiteAndTests.getKey();
+          List<TestData> tests = suiteAndTests.getValue();
+          if (suite.getResult() != null
+              && suite.getResult().getStatus() != TestExecutionResult.Status.SUCCESSFUL) {
+            // If a test suite fails or is skipped, all its tests must be included in the XML output
+            // with the same result as the suite, since the XML format does not support marking a
+            // suite as failed or skipped. This aligns with Bazel's XmlWriter for JUnitRunner.
+            getTestsFromSuite(suite.getId())
+                .forEach(
+                    testIdentifier -> {
+                      TestData test = results.get(testIdentifier.getUniqueIdObject());
+                      if (test == null) {
+                        // add test to results.
+                        test = getResult(testIdentifier);
+                        tests.add(test);
+                      }
+                      test.mark(suite.getResult()).skipReason(suite.getSkipReason());
+                    });
+          }
+          new TestSuiteXmlRenderer(testPlan).toXml(xml, suite, tests);
         }
       } catch (XMLStreamException e) {
         throw new RuntimeException(e);
@@ -225,6 +250,18 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
       Stream.concat(testCases.stream(), testSuites.keySet().stream())
           .forEach(data -> results.remove(data.getId().getUniqueIdObject()));
     }
+  }
+
+  private List<TestIdentifier> getTestsFromSuite(TestIdentifier suiteIdentifier) {
+    return testPlan.getChildren(suiteIdentifier).stream()
+        .flatMap(
+            testIdentifier -> {
+              if (testIdentifier.isContainer()) {
+                return getTestsFromSuite(testIdentifier).stream();
+              }
+              return Stream.of(testIdentifier);
+            })
+        .collect(Collectors.toList());
   }
 
   @Override
@@ -248,13 +285,50 @@ public class BazelJUnitOutputListener implements TestExecutionListener, Closeabl
       return;
     }
     if (wasInterrupted.get()) {
-      output(true);
+      output(true, null);
     }
     try {
       xml.writeEndDocument();
       xml.close();
     } catch (XMLStreamException e) {
       LOG.log(Level.SEVERE, "Unable to close xml output", e);
+    }
+  }
+
+  static class LazyFileWriter extends Writer {
+    private final Path path;
+    private BufferedWriter delegate;
+    private boolean isCreated = false;
+
+    public LazyFileWriter(Path path) {
+      this.path = path;
+    }
+
+    private void createWriterIfNeeded() throws IOException {
+      if (!isCreated) {
+        delegate = Files.newBufferedWriter(path, UTF_8);
+        isCreated = true;
+      }
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+      createWriterIfNeeded();
+      delegate.write(cbuf, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+      if (isCreated) {
+        delegate.flush();
+      }
+    }
+
+    @Override
+    public void close() throws IOException {
+      if (isCreated) {
+        delegate.close();
+      }
     }
   }
 }

java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/StubbedTestDescriptor.java
@@ -11,9 +11,15 @@ import org.junit.platform.engine.UniqueId;
 public class StubbedTestDescriptor implements TestDescriptor {
 
   private final UniqueId uniqueId;
+  private final Type type;
 
   public StubbedTestDescriptor(UniqueId uniqueId) {
+    this(uniqueId, Type.TEST);
+  }
+
+  public StubbedTestDescriptor(UniqueId uniqueId, Type type) {
     this.uniqueId = uniqueId;
+    this.type = type;
   }
 
   @Override
@@ -68,7 +74,7 @@ public class StubbedTestDescriptor implements TestDescriptor {
 
   @Override
   public Type getType() {
-    return Type.TEST;
+    return type;
   }
 
   @Override

java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/BazelJUnitOutputListenerTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 import static org.junit.platform.launcher.LauncherConstants.STDERR_REPORT_ENTRY_KEY;
 import static org.junit.platform.launcher.LauncherConstants.STDOUT_REPORT_ENTRY_KEY;
+import static org.mockito.Mockito.when;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.File;
@@ -17,8 +18,10 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -28,6 +31,7 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.UniqueId;
@@ -256,6 +260,57 @@ public class BazelJUnitOutputListenerTest {
     Node failure = failureZero == null ? failureOne : failureZero;
 
     assertEquals("Test timed out and was interrupted", failure.getTextContent());
+  }
+
+  @Test
+  void testSuiteError(@TempDir Path tempDir)
+      throws ParserConfigurationException, IOException, SAXException {
+    TestIdentifier root =
+        TestIdentifier.from(new StubbedTestDescriptor(UniqueId.parse("[engine:mocked]")));
+    TestIdentifier suiteIdentifier =
+        TestIdentifier.from(
+            new StubbedTestDescriptor(
+                UniqueId.parse("[engine:mocked]/[class:ExampleTest]"),
+                TestDescriptor.Type.CONTAINER));
+    TestData suite =
+        new TestData(suiteIdentifier)
+            .started()
+            .mark(TestExecutionResult.failed(new RuntimeException("test suite error")));
+    TestPlan testPlan = Mockito.mock(TestPlan.class);
+
+    TestIdentifier child1 = TestIdentifier.from(new StubbedTestDescriptor(createId("child1")));
+    TestIdentifier child2 = TestIdentifier.from(new StubbedTestDescriptor(createId("child2")));
+
+    when(testPlan.getChildren(suite.getId())).thenReturn(Set.of(child1, child2));
+    when(testPlan.getRoots()).thenReturn(Set.of(root));
+
+    Path xmlPath = tempDir.resolve("test.xml");
+    try (BazelJUnitOutputListener listener = new BazelJUnitOutputListener(xmlPath)) {
+      listener.testPlanExecutionStarted(testPlan);
+      listener.executionStarted(root);
+      listener.executionStarted(suiteIdentifier);
+      listener.executionFinished(
+          suiteIdentifier, TestExecutionResult.failed(new RuntimeException("test suite error")));
+      listener.executionFinished(root, TestExecutionResult.successful());
+    }
+
+    Document document =
+        DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlPath.toFile());
+    document.getDocumentElement().normalize();
+
+    NodeList testsuiteList = document.getDocumentElement().getElementsByTagName("testsuite");
+    assertEquals(1, testsuiteList.getLength());
+    Element testsuite = (Element) testsuiteList.item(0);
+    assertEquals("2", testsuite.getAttribute("tests"));
+    assertEquals("2", testsuite.getAttribute("failures"));
+
+    NodeList testcaseList = testsuite.getElementsByTagName("testcase");
+    assertEquals(2, testcaseList.getLength());
+
+    for (int i = 0; i < testcaseList.getLength(); i++) {
+      Element testcase = (Element) testcaseList.item(i);
+      assertNotNull(getChild("failure", testcase));
+    }
   }
 
   @Test