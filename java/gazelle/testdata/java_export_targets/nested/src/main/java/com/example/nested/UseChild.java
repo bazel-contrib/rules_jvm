d78fbe170513537b14fe5e2bb6f73cff1897766d
Borja Lorente <blorente.me@gmail.com>
2025-06-20 11:53:25 +0100

    Fix more tests

--- java/gazelle/testdata/java_export_targets/nested/src/main/java/com/example/nested/UseChild.java
+++ java/gazelle/testdata/java_export_targets/nested/src/main/java/com/example/nested/UseChild.java
@@ -1,5 +1,7 @@
 package com.example.nested;
 
+import com.example.nested.child_export.ChildExport;
+
 public class NestedExport {
   public static String module() {
     return "Parent of " + ChildExport.module();
