5ba4ba2b4e44eab9201c53cb5d87e87499f6cc28
Ted Kaplan <tkaplan@roku.com>
2025-06-22 11:01:18 -0700

    Ensure deps referenced in java_test_suite make vars are passed to tests (#332)
    https://github.com/bazel-contrib/rules_jvm/pull/303 introduced a
    regression where if a `java_test_suite` references a dep in its
    jvm_flags via `$(location)` or other Make variable, it will not be
    present in the deps of the generated `java_test` leading to analysis
    errors. This PR adds an example demonstrating the issue and suggests a
    possible fix.
    
    Fixes #334

---
+++ examples/tests_and_lints/src/test/com/github/bazel_contrib/contrib_rules_jvm/examples/tests_and_lints/suite/ExampleTest.java
@@ -0,0 +1,8 @@
+package com.github.bazel_contrib.contrib_rules_jvm.examples.tests_and_lints.suite;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+  @Test
+  public void passes() {}
+}
