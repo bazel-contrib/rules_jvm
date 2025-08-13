8d73e633d3c78391535d5b933b278caa1f2d13c6
Christian Adam <60895224+christianladam@users.noreply.github.com>
2025-08-13 09:03:56 -0700

    chore: add parameterized and repeated tests to the junit5 test target (#352)
    Also generated the comparative xml for the repeated test

---
+++ comparative-tests/src/test/java/com/github/bazel_contrib/contrib_rules_jvm/comparative/RepeatsTest.java
@@ -0,0 +1,11 @@
+package com.github.bazel_contrib.contrib_rules_jvm.comparative;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+class RepeatsTest {
+
+  @RepeatedTest(3)
+  public void bootstrap() {
+    // do nothing
+  }
+}
