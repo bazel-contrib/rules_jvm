9c611ce9cce36b2618361f3fccd73cd8aa6dcf5f
Evan Loriot <12630925+evanloriot@users.noreply.github.com>
2025-11-15 11:14:55 -0800

    Fix shutdown hooks for JUnit5 for JDK24+. Previously, all tests would exit with System.exit or Runtime.exit was called because the shutdown hook was not removed at test teardown.

--- java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/NullSystemExitToggle.java
+++ java/src/com/github/bazel_contrib/contrib_rules_jvm/junit5/NullSystemExitToggle.java
@@ -1,13 +1,32 @@
 package com.github.bazel_contrib.contrib_rules_jvm.junit5;
 
+import java.util.Optional;
+
 public class NullSystemExitToggle implements SystemExitToggle {
+
+  private Optional<Thread> shutdownHook;
+
+  public NullSystemExitToggle() {
+    this.shutdownHook = Optional.empty();
+  }
+
   @Override
   public void prevent() {
-    // No-op
+    if (!shutdownHook.isEmpty()) {
+      throw new RuntimeException("SystemExitDetectingShutdownHook already added");
+    }
+    // Install a shutdown hook so people can track down what went wrong
+    // if a test calls `System.exit`
+    Thread shutdownHook = SystemExitDetectingShutdownHook.newShutdownHook(System.err);
+    this.shutdownHook = Optional.of(shutdownHook);
+    Runtime.getRuntime().addShutdownHook(shutdownHook);
   }
 
   @Override
   public void allow() {
-    // No-op
+    if (shutdownHook.isEmpty()) {
+      throw new RuntimeException("SystemExitDetectingShutdownHook never added");
+    }
+    Runtime.getRuntime().removeShutdownHook(shutdownHook.get());
   }
 }
