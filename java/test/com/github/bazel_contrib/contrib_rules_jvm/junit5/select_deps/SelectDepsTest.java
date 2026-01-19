50bba0051aa37a92520ebb28eb7dcb82f7fc7de7
Simon Mavi Stewart <shs@squareup.com>
2026-01-19 10:13:46 +0000

    Run format

--- java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/select_deps/SelectDepsTest.java
+++ java/test/com/github/bazel_contrib/contrib_rules_jvm/junit5/select_deps/SelectDepsTest.java
@@ -1,13 +1,14 @@
 package com.github.bazel_contrib.contrib_rules_jvm.junit5.select_deps;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /** Test that verifies java_test_suite works with select() in deps. */
 class SelectDepsTest {
 
-    @Test
-    void testSelectDepsWork() {
-        assertTrue(true, "Test suite with select() deps should load and run");
-    }
+  @Test
+  void testSelectDepsWork() {
+    assertTrue(true, "Test suite with select() deps should load and run");
+  }
 }
