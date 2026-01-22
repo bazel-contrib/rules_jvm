5ca39be3e550ece498f4ce397ebc9b883e1a4fb8
Simon Mavi Stewart <simon.m.stewart@gmail.com>
2026-01-22 14:18:34 +0000

    [gazelle]: Support the new `rules_jvm_external` lock file format (#408)
    There's a new lock file in town, and for us, it looks almost exactly the
    same as the old one. Adding support for it to the plugin.

--- java/gazelle/private/maven/config_test.go
+++ java/gazelle/private/maven/config_test.go
@@ -81,3 +81,41 @@ func Test_loadConfiguration_v2(t *testing.T) {
 		"com.google.thirdparty.publicsuffix",
 	})
 }
+
+func Test_loadConfiguration_v3(t *testing.T) {
+	cfg, err := loadConfiguration("testdata/v3_maven_install.json")
+	require.NoError(t, err)
+
+	require.ElementsMatch(t, cfg.ListDependencies(), []string{
+		"com.google.code.findbugs:jsr305",
+		"com.google.errorprone:error_prone_annotations",
+		"com.google.guava:failureaccess",
+		"com.google.guava:guava",
+		"com.google.guava:listenablefuture",
+		"com.google.j2objc:j2objc-annotations",
+		"org.checkerframework:checker-qual",
+	})
+
+	require.Equal(t, cfg.GetDependencyCoordinates("com.google.guava:guava"), "com.google.guava:guava:31.1-jre")
+
+	require.ElementsMatch(t, cfg.ListDependencyPackages("com.google.guava:guava"), []string{
+		"com.google.common.annotations",
+		"com.google.common.base.internal",
+		"com.google.common.base",
+		"com.google.common.cache",
+		"com.google.common.collect",
+		"com.google.common.escape",
+		"com.google.common.eventbus",
+		"com.google.common.graph",
+		"com.google.common.hash",
+		"com.google.common.html",
+		"com.google.common.io",
+		"com.google.common.math",
+		"com.google.common.net",
+		"com.google.common.primitives",
+		"com.google.common.reflect",
+		"com.google.common.util.concurrent",
+		"com.google.common.xml",
+		"com.google.thirdparty.publicsuffix",
+	})
+}
